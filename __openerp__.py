3a5a42bfbe0daf0fffaccfe0d6f540c93afba094
Valentin THIRION <valentinthirion@gmail.com>
2015-02-25 09:22:44 +0100

    Improved description

--- __openerp__.py
+++ __openerp__.py
@@ -11,8 +11,6 @@ It adds:
 	- Team management for contracts, allows to create teams with users and link them to contracts
 	- Type management for contracts, allows to create types with a product, a price and link them to contracts
 	- BL invoicing: enable to invoice regarding the AbAKUS invoicing policy in BL Support contracts
-	- New management of issues stages
-		- Auto set assigned stage when issue assigned
 
 This module has been developed by Bernard Delhez, intern @ AbAKUS it-solutions, under the control of Valentin Thirion.""",
     'data': ['account_analytic_account_view.xml','security/ir.model.access.csv',],
